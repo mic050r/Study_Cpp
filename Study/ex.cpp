e1efbdbc9f04151e1518bd7e15daf696deaf33c1
mic050r <103114387+mic050r@users.noreply.github.com>
2023-05-22 23:20:42 +0900

    [C++] Exercise 3-1

---
+++ Study/ex.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; //cout 객체는 std동네의 cout을 말하는 것
+//01, 02 => cout객체를 사용하기 위한 준비 작업
+int main() {
+	cout << "what's your name?\n"; //cout : console output의 약자 << : 화살표 느낌
+	//EX3-1
+	cout << "#######################################\n";
+	cout << "#임지현 프로그래밍의 세계에 발 담그다!#\n";
+	cout << "#######################################\n";
+	return 0;
+}
