8074ed8db4939b3ba3054c4afc6d2d83235bc640
mic050r <103114387+mic050r@users.noreply.github.com>
2023-05-22 23:29:26 +0900

    [C++] 구조체 정의

--- Study/ex10-1.cpp
+++ Study/ex10-1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// StudentInfo라는 이름의 구조체를 정의
 struct StudentInfo
 {
 	char bloodType; // 혈액형
@@ -9,22 +10,8 @@ struct StudentInfo
 };
 
 int main() {
+	// StudentInfo 구조체 타입의 변수 2개를 선언
 	StudentInfo si1;
 	StudentInfo si2;
-
-	si1.bloodType = 'O';
-	si1.stdnumber = 20031128;
-	si1.grade = 3.5f;
-
-	si2.bloodType = 'A';
-	si2.stdnumber = 19961219;
-	si2.grade = 2.3f;
-
-	cout << "학번 : " << si1.stdnumber << "\n";
-	cout << "학점 : " << si1.grade << "\n";
-	cout << "혈액행 : " << si1.bloodType << "\n\n";
-	cout << "학번 : " << si2.stdnumber << "\n";
-	cout << "학점 : " << si2.grade << "\n";
-	cout << "혈액행 : " << si2.bloodType << "\n\n";
 	return 0;
 }
